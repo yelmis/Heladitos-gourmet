e4d846ba92e38ab4f87483bd63160bb5dc642bbb
yelmis <pichardoyelmis7@gmail.com>
2024-11-19 09:58:49 -0400

    app de la pagina

--- JS/app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('navLinks');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
