1d4fe01fde7b46c35fc58f7e590be1a65a5eba80
yelmis <pichardoyelmis7@gmail.com>
2024-11-21 15:17:11 -0400

    aplicacion de una alerta y navegador 21 de nov

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+const aleta = alert('Bienvenido a nuestros pagina web')
